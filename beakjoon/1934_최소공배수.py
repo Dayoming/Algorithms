42743ee5e83dd414c3e11d5c2285046554402831
Dayoming <jdayoung1204@naver.com>
2022-01-31 21:15:43 +0900

    1934번 최소공배수 업로드. 유클리드 호제법

---
+++ beakjoon/1934_최소공배수.py
@@ -0,0 +1,21 @@
+# 유클리드 호제법을 사용해서 최대공약수를 구한다
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    if b == 0:
+        return a
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a%b)
+
+# 두 수를 곱한 값을 최대공약수로 나눈다
+def lcm(a, b):
+    result = (a * b) // gcd(a, b)
+    return result
+
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    print(lcm(a, b))
