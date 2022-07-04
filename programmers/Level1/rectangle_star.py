4df1104469441a12a7bb2fc2a2281773123ae468
Dayoming <jdayoung1204@naver.com>
2022-07-04 15:19:26 +0900

    for문 변수명 변경

--- programmers/Level1/rectangle_star.py
+++ programmers/Level1/rectangle_star.py
@@ -1,6 +1,6 @@
 # Rectangle horizon and vertical input
 horizon, vertical = map(int, input().strip().split(' '))
-for c in range(0, vertical):  # repeat as column
-    for r in range(0, horizon):  # repeat as horizon
+for v in range(0, vertical):  # repeat as column
+    for h in range(0, horizon):  # repeat as horizon
         print('*', end='')  # Output as many horizontal iterations
     print()  # new line
