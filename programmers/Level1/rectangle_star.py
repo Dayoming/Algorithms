09492d6c8d44c25e8fff0fb890ae78a56f580ecc
Dayoming <jdayoung1204@naver.com>
2022-07-04 15:29:35 +0900

    Edit comments

--- programmers/Level1/rectangle_star.py
+++ programmers/Level1/rectangle_star.py
@@ -1,6 +1,6 @@
 # Rectangle horizon and vertical input
 horizon, vertical = map(int, input().strip().split(' '))
-for v in range(0, vertical):  # repeat as column
+for v in range(0, vertical):  # repeat as vertical
     for h in range(0, horizon):  # repeat as horizon
         print('*', end='')  # Output as many horizontal iterations
     print()  # new line
