c084a32ba0ae738c01c094293541fdb674d471fd
Dayoming <jdayoung1204@naver.com>
2023-01-18 03:23:19 +0900

    백준 10845번 큐 업로드

---
+++ beakjoon/단계별 풀기/큐, 덱/10845_큐.py
@@ -0,0 +1,35 @@
+# 큐
+import sys
+# 데이터를 양방향에서 추가하고 제거할 수 있다
+from collections import deque
+
+n = int(sys.stdin.readline())
+queue = deque()
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
