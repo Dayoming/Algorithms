67e8c34fd18d3ad36fc640543f0f7bc9781fe4a8
Dayoming <jdayoung1204@naver.com>
2023-02-15 15:49:51 +0900

    프로그래머스 다리를 지나는 트럭 두 번째 아이디어

--- programmers/Level2/다리를 지나는 트럭.py
+++ programmers/Level2/다리를 지나는 트럭.py
@@ -7,26 +7,25 @@ def solution(bridge_length, weight, truck_weights):
     # 대기 중인 트럭
     trucks = deque(truck_weights)
     # 다리를 건너고 있는 중인 트럭
-    bridge = deque()
+    bridge = deque([0] * bridge_length)
     # 다리를 건너고 있는 중인 트럭 무게의 합
     bridge_weight = 0
 
-    # 대기하고 있는 트럭이 있는 동안
-    while trucks:
-        # 만약 다리를 건너는 중인 트럭 무게 + 다리를 건널 트럭 무게가 무게 제한보다 작다면
-        if bridge_weight + trucks[0] <= weight:
-            truck_weight = trucks.popleft()
-            # 다리에 트럭 한 대를 더해준다
-            bridge.append(truck_weight)
-            # 다리를 건너고 있는 중인 트럭 무게의 합에 트럭 무게를 더해준다
-            bridge_weight += truck_weight
-            # 시간 + 1
-            answer += 1
-        else:
-            # 제한보다 크다면, 다리에서 트럭 하나를 보낸다
-            bridge_weight -= bridge.popleft()
-            # 시간 + 1
-            answer += 1
+    while bridge:
+        bridge_weight -= bridge.popleft()
+        # 대기중인 트럭이 존재하면
+        if trucks:
+            # 만약 다리를 건너는 중인 트럭 무게 + 다리를 건널 트럭 무게가 무게 제한보다 작다면
+            if bridge_weight + trucks[0] <= weight:
+                truck_weight = trucks.popleft()
+                # 다리에 트럭 한 대를 더해준다
+                bridge.append(truck_weight)
+                # 다리를 건너고 있는 중인 트럭 무게의 합에 트럭 무게를 더해준다
+                bridge_weight += truck_weight
+            else:
+                # 제한보다 크다면 0을 추가해 다리 길이를 유지한다
+                bridge.append(0)
+        answer += 1
 
     return answer
 
