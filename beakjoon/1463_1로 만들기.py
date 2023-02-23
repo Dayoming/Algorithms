c05d4c11ec7a596118aa8df7364ffa9942afe786
Dayoming <jdayoung1204@naver.com>
2023-02-23 14:43:20 +0900

    백준 1463번 1로 만들기 업로드

---
+++ beakjoon/1463_1로 만들기.py
@@ -0,0 +1,15 @@
+# 다이나믹 프로그래밍
+import sys
+
+x = int(sys.stdin.readline())
+dp = [0] * (x + 1)
+
+for i in range(2, x+1):
+    dp[i] = dp[i-1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+
+print(dp[x])
