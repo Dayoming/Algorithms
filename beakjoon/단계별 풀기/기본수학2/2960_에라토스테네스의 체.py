a8d7841ed53dbfb34209f723df2844ea73171fe6
Dayoming <jdayoung1204@naver.com>
2023-02-25 10:58:19 +0900

    백준 2960번 에라토스테네스의 체 업로드

---
+++ beakjoon/단계별 풀기/기본수학2/2960_에라토스테네스의 체.py
@@ -0,0 +1,17 @@
+# 수학, 구현, 정수론, 소수 판정, 에라토스테네스의 체
+
+def get_prime(N, K):
+    count = 0
+    arr = [True] * (N + 1)
+    for i in range(2, N):
+        if arr[i] == True: # 만약 i가 소수라면
+            for j in range(i, N + 1, i):
+                # 이미 지워진 수가 아니라면
+                if arr[j] != False:
+                    arr[j] = False
+                    count += 1
+                if count == K:
+                    return(j)
+
+N, K = map(int, input().split())
+print(get_prime(N, K))
