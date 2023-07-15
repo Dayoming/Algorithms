bedbf026ee925f8d4cfd1a6f4f3c1294accf5d22
Dayoming <jdayoung1204@naver.com>
2023-07-16 03:55:30 +0900

    Leetcode Medium 6. Zigzag Conversion Upload

---
+++ leetcode/Medium/Zigzag Conversion.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        result = [''] * numRows
+        index, step = 0, 1
+
+        for char in s:
+            result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return ''.join(result)
+
+s = Solution()
+s.convert("PAYPALISHIRING", 3)
+s.convert("PAYPALISHIRING", 4)
